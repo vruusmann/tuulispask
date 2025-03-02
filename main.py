85d5ef80201e705f2a826451cb7fe4e47f514dec
Villu Ruusmann <villu.ruusmann@gmail.com>
2025-03-02 12:12:55 +0200

    Fixed wind speed units
    The default is "kmh", whereas humans typically speak in terms of "ms"

--- main.py
+++ main.py
@@ -23,44 +23,43 @@ if len(sys.argv) > 1:
 # Set location coordinates
 latitude, longitude = coordinates[location]
 
-# Calculate dates for past year
-end_date = datetime.now() - timedelta(days=2)
-start_date = end_date - timedelta(days=365)
-
-start_date_str = start_date.strftime('%Y-%m-%d')
-end_date_str = end_date.strftime('%Y-%m-%d')
-
 wind_height = "100m"
 if len(sys.argv) > 2:
 	wind_height = sys.argv[2]
 
-def fetch_history(type, variable):
+def fetch_history(id, type, variable, start_date, end_date):
 	url = "https://archive-api.open-meteo.com/v1/archive"
 	params = {
 		"latitude": latitude,
 		"longitude": longitude,
-		"start_date" : start_date_str,
-		"end_date" : end_date_str
+		"start_date" : start_date,
+		"end_date" : end_date,
+		"windspeed_unit" : "ms"
 	}
 	params[type] = variable
 
 	response = requests.get(url, params = params)
+	print("URL: {}".format(response.url))
+
+	with open("data/{id}.json".format(id = id), "wb") as file:
+		file.write(response.content)
+
 	data = response.json()
 
 	return data[type][variable]
 
-def wind_direction():
+def wind_direction(id, start_date, end_date):
 	type = "hourly"
 	variable = "winddirection_{wind_height}".format(wind_height = wind_height)
-	label = "Hourly Wind Direction at {wind_height} (degrees)".format(wind_height = wind_height)
+	title = "Hourly Wind Direction at {wind_height} in degrees ({start_date} to {end_date})".format(wind_height = wind_height, start_date = start_date, end_date = end_date)
 
-	values = fetch_history(type, variable)
+	values = fetch_history(id, type, variable, start_date, end_date)
 
-	plt = wind_direction_plot(values, label)
-	plt.savefig("plots/{location}_{type}_{variable}.png".format(location = location, type = type, variable = variable))
+	plt = wind_direction_plot(values, title)
+	plt.savefig("plots/{id}.png".format(id = id))
 	plt.show()
 
-def wind_direction_plot(values, label):
+def wind_direction_plot(values, title):
 	fig = plt.figure(figsize=(12, 12))
 	ax = fig.add_subplot(111, projection='polar')
 
@@ -85,7 +84,7 @@ def wind_direction_plot(values, label):
 	ax.set_xticklabels(['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'])
 
 	# Add title
-	plt.title(label, pad=20, fontsize=18)
+	plt.title(title, pad=20, fontsize=18)
 
 	# Add grid
 	ax.yaxis.grid(True)
@@ -94,36 +93,40 @@ def wind_direction_plot(values, label):
 
 	return plt
 
-def wind_speed():
+def wind_speed(id, start_date, end_date):
 	type = "hourly"
 	variable = "wind_speed_{wind_height}".format(wind_height = wind_height)
-	label = "Hourly Wind Speed at {wind_height} (m/s)".format(wind_height = wind_height)
+	title = "Hourly Wind Speed at {wind_height} in m/s ({start_date} to {end_date})".format(wind_height = wind_height, start_date = start_date, end_date = end_date)
 
-	values = fetch_history(type, variable)
+	values = fetch_history(id, type, variable, start_date, end_date)
 
-	plt = wind_speed_plot(values, label)
-	plt.savefig("plots/{location}_{type}_{variable}.png".format(location = location, type = type, variable = variable))
+	plt = wind_speed_plot(values, title)
+	plt.savefig("plots/{id}.png".format(id = id))
 	plt.show()
 
-def wind_speed_plot(values, label):
-	quantiles = numpy.quantile(values, [0.05, 0.20, 0.50, 0.80, 0.95]).tolist()
-	print("Quantiles: {}".format(quantiles))
+def wind_speed_plot(values, title):
+	quantiles = None
+
+	if len(values) > 100:
+		quantiles = numpy.quantile(values, [0.05, 0.20, 0.50, 0.80, 0.95]).tolist()
+		print("Quantiles: {}".format(quantiles))
 
 	# Create histogram
 	plt.figure(figsize=(12, 8))
 	plt.hist(values, bins=30, edgecolor='black')
 
-	for quantile in quantiles:
-		if quantile in (quantiles[0], quantiles[-1]):
-			color = "red"
-			linestyle = "-"
-		else:
-			color = "black"
-			linestyle = "--"
-		plt.axvline(x=quantile, color=color, linestyle=linestyle)
+	if quantiles:
+		for quantile in quantiles:
+			if quantile in (quantiles[0], quantiles[-1]):
+				color = "red"
+				linestyle = "-"
+			else:
+				color = "black"
+				linestyle = "--"
+			plt.axvline(x=quantile, color=color, linestyle=linestyle)
 
-	plt.title(f'{label} Distribution ({start_date_str} to {end_date_str})'.format(label = label), fontsize=18)
-	plt.xlabel('{label}'.format(label = label), fontsize=16)
+	plt.title(title, fontsize=18)
+	plt.xlabel('Wind Speed (m/s)', fontsize=16)
 	plt.ylabel('Frequency', fontsize=16)
 	plt.grid(True, alpha=0.3)
 
@@ -131,5 +134,12 @@ def wind_speed_plot(values, label):
 
 	return plt
 
-wind_direction()
-wind_speed()
+# Calculate dates for past year
+end_date = datetime.now() - timedelta(days=2)
+start_date = end_date - timedelta(days=365)
+
+start_date_str = start_date.strftime('%Y-%m-%d')
+end_date_str = end_date.strftime('%Y-%m-%d')
+
+wind_direction("{location}_wind_direction_past_year".format(location = location), start_date_str, end_date_str)
+wind_speed("{location}_wind_speed_past_year".format(location = location), start_date_str, end_date_str)
